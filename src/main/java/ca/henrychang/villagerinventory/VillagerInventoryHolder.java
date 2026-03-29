3551aed71109852425df923d96ecf63a535378c8
Draconic Velum <87547775+DraconicVelum@users.noreply.github.com>
2026-03-29 16:41:49 +0100

    Add files via upload

---
+++ src/main/java/ca/henrychang/villagerinventory/VillagerInventoryHolder.java
@@ -0,0 +1,12 @@
+package ca.henrychang.villagerinventory;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class VillagerInventoryHolder implements InventoryHolder {
+
+    @Override
+    public Inventory getInventory() {
+        return null; // not used
+    }
+}
